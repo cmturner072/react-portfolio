ae573fc8e6b7c93527e82c72509a3d94d21d99bb
MatheStine <cmturner072@gmail.com>
2020-11-30 16:49:13 -0600

    updates

---
+++ src/components/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PortfolioContainter from "../portfolio/portfolio-container";
+
+export default function home() {
+    return (
+        <div>
+            <h2>Homepage</h2>
+            <PortfolioContainter />
+        </div>
+    )
+}
