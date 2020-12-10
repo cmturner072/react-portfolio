ab149dc3eae23cf7b9764ce88fdda83138286d1e
MatheStine <cmturner072@gmail.com>
2020-12-10 16:27:29 -0600

    login

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export default class NavigationComponent extends Component {
-    constructor() {
-        super();
-    }
+const NavigationComponent = (props) => {
+    
 
-    render() {
+
         return (
             <div className="nav-wrapper">
                 <div className="left-side">
@@ -38,4 +36,4 @@ export default class NavigationComponent extends Component {
             </div>
         )
     }
-}
+export default NavigationComponent;
