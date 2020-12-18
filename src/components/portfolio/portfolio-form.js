a9c2dbb58f96ae3e6d6b704119da5f70ac8ac015
MatheStine <cmturner072@gmail.com>
2020-12-18 16:02:29 -0600

    Functional Form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from "axios";
+
 
 export default class PortfolioForm extends Component {
     constructor(props) {
@@ -41,7 +43,13 @@ export default class PortfolioForm extends Component {
         }
 
         handleSubmit(event) {
-            this.buildForm();
+            axios.post("https://christineturner.devcamp.space/portfolio/portfolio_items", 
+            this.buildForm(), { withCredentials: true }
+            ).then(response => {
+                console.log("response", response);
+            }).catch(error => {
+                console.log("portfolio form handleSubmit error", error);
+            })
             event.preventDefault();
         }
     render() {
