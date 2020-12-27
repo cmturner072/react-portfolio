a066fb1eeaecf218240ef2bce1b280ef7c246049
MatheStine <cmturner072@gmail.com>
2020-12-27 16:19:47 -0600

    Font awesome added

--- src/components/portfolio/portfolio-sidebar-list.js
+++ src/components/portfolio/portfolio-sidebar-list.js
@@ -1,3 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 
@@ -8,9 +9,14 @@ const PortfolioSidebarList = (props) => {
                <div className="portfolio-thumb-img">
                    <img src={portfolioItem.thumb_image_url} />
                </div>
-               <h1 className="title">{portfolioItem.name}</h1>
-               <h2>{portfolioItem.id}</h2>
-               <a onClick={() => props.handleDeleteClick(portfolioItem)}>Delete</a>       
+               
+               <div className="text-content">
+                    <div className="title">{portfolioItem.name}</div>
+                    <a className="delete-icon" onClick={() => props.handleDeleteClick(portfolioItem)}>
+                        <FontAwesomeIcon icon="trash" />
+                    </a>
+               </div>
+                     
            </div>
        ); 
     })
