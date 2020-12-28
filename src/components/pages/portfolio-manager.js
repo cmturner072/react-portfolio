87f2e28237ae4d09f65015a653c2fc68cc7f22dd
MatheStine <cmturner072@gmail.com>
2020-12-28 17:15:22 -0600

    Edit icon added

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -10,12 +10,20 @@ export default class PortfolioManager extends Component {
         super();
 
         this.state = {
-            portfolioItems: []
+            portfolioItems: [],
+            portfolioToEdit: {}
         };
 
         this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(this);
         this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this)
         this.handleDeleteClick = this.handleDeleteClick.bind(this);
+        this.handleEditClick = this.handleEditClick.bind(this);
+    }
+
+    handleEditClick(portfolioItem) {
+        this.setState ({
+            portfolioToEdit: portfolioItem 
+        })
     }
 
     handleDeleteClick(portfolioItem) {
@@ -73,6 +81,7 @@ export default class PortfolioManager extends Component {
                     <PortfolioSidebarList
                     handleDeleteClick={this.handleDeleteClick}
                     data={this.state.portfolioItems}
+                    handleEditClick={this.handleEditClick}
                     />  
                </div>
             </div>
