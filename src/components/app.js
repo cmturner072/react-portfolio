8cee561df5fce1a61c8c801e64177b38609858b4
MatheStine <cmturner072@gmail.com>
2021-01-05 16:06:03 -0600

    blog link clickable

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
@@ -116,15 +117,14 @@ export default class App extends Component {
             <Route path="/about-me" component={About} />
             <Route path="/contact" component={Contact} />
             <Route path="/blog" component={Blog} />
+            <Route path="/b/:slug" component={BlogDetail} />
             {this.state.loggedInStatus ===  "LOGGED_IN" ? this.authorizedPages() : null}
             <Route exact path="/portfolio/:slug" component={PortfolioDetail} />
             <Route component={NoMatch}/>
           </Switch>
           
         </div>
-      </Router>
-
-        
+      </Router>        
 
       </div>
     );
