86de4ef0331ad51315783e4b915eb69448df6dcf
MatheStine <cmturner072@gmail.com>
2021-01-09 16:31:23 -0600

    spinner added

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import axios from 'axios';
 import BlogItem from '../blog/blog-item';
 
@@ -9,7 +10,10 @@ constructor() {
     super();
 
     this.state = {
-        blogItems: []
+        blogItems: [],
+        totalCount: 0,
+        currentPage: 0,
+        isLoading: true
     }
 
     this.getBlogItems = this.getBlogItems.bind(this);
@@ -26,11 +30,17 @@ constructor() {
     }
 
     getBlogItems() {
+        this.setState({
+            currentPage: this.state.currentPage + 1
+        });
+
         axios.get("https://christineturner.devcamp.space/portfolio/portfolio_blogs", { 
             withCredentials: true 
         }).then(response => {
             this.setState({
-                blogItems: response.data.portfolio_blogs
+                blogItems: response.data.portfolio_blogs,
+                totalCount: response.data.meta.total_records,
+                isLoading: false
             });
         }).catch(error => {
             console.log("getBlogItems error", error)
@@ -53,6 +63,13 @@ constructor() {
                 <div className="content-container">
                     {blogRecords}
                 </div>
+
+                {this.state.isLoading ? (
+                    <div className="content-loader">
+                    <FontAwesomeIcon icon="spinner" spin />
+                </div>
+                ) : null }
+                
             </div>
         )
     }
