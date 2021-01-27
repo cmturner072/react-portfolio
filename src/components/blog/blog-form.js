10515f40dd0845ec01e13f9a2c5e5a3751c68882
MatheStine <cmturner072@gmail.com>
2021-01-27 16:18:51 -0600

    Login added to edit blog

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -14,6 +14,8 @@ export default class BlogForm extends Component {
             blog_status: "",
             content: "",
             featured_image: "",
+            apiUrl: "https://christineturner.devcamp.space/portfolio/portfolio_blogs",
+            apiAction: "post",
 
         }
 
@@ -47,7 +49,10 @@ export default class BlogForm extends Component {
           this.setState({
               id: this.props.blog.id,
               title: this.props.blog.title,
-              status: this.props.blog.status
+              blog_status: this.props.blog.blog_status,
+              content: this.props.blog.content,
+              apiUrl: `https://christineturner.devcamp.space/portfolio/portfolio_blogs/${this.props.blog.id}`,
+              apiAction: "patch"
           })
       }  
     }
@@ -97,11 +102,12 @@ export default class BlogForm extends Component {
 
     
     handleSubmit(event) {
-        axios.post(
-            "https://christineturner.devcamp.space/portfolio/portfolio_blogs", 
-            this.buildForm(), 
-            { withCredentials: true }
-            )
+        axios({
+            method: this.state.apiAction,
+            url: this.state.apiUrl,
+            data: this.buildForm(),
+            withCredentials: true
+          })
             .then(response => {
                 if (this.state.featured_image) {
                     this.featuredImageRef.current.dropzone.removeAllFiles();
@@ -114,11 +120,14 @@ export default class BlogForm extends Component {
                     featured_image: ""
                 });
                 
+                if (this.props.editMode) {
+                    // Update blog detail
+                    this.props.handleUpdateFormSubmission(response.data.portfolio_blog);
+                } else {
                 this.props.handleSuccessfullFormSubmission(
                     response.data.portfolio_blog
                 );
-
-                
+                }                
             }).catch(error => {
                console.log("handleSubmit for blog error", error) 
             })
--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -18,7 +18,14 @@ export default class BlogDetail extends Component {
 
         this.handleEditClick = this.handleEditClick.bind(this);
         this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+        this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(this);
+    }
 
+    handleUpdateFormSubmission(blog) {
+        this.setState({
+            blogItem: blog,
+            editMode: false
+        })
     }
 
     handleFeaturedImageDelete() {
@@ -31,7 +38,9 @@ export default class BlogDetail extends Component {
 
     handleEditClick() {
         console.log("handle edit clicked");
+        if (this.props.loggedInStatus === "LOGGED_IN") {
         this.setState({ editMode: true });
+        }
     }
 
     getBlogItem() {
@@ -58,12 +67,15 @@ export default class BlogDetail extends Component {
         
         const contentManager = () => {
             if (this.state.editMode) {
-                return <BlogForm 
-                handleFeaturedImageDelete={this.handleFeaturedImageDelete} 
+                return ( 
+                <BlogForm 
+                handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+                handleUpdateFormSubmission={this.handleUpdateFormSubmission}
                 editMode={this.state.editMode} 
                 blog={this.state.blogItem} 
                     
-                />;
+                />
+                );
             } else {
                 return(
                     <div className="content-container">
